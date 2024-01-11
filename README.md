5f8994faad63ae5acecac174e1195e3f3b713395
tor first <victoriah.first@gmail.com>
2024-01-10 23:48:47 -0600

    updated README and added video walkthrough

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-# svglogomaker
+# SVG Logo Maker
 
-<!-- App description, link to video, setup and usage instructions -->
+## Description
+
+This project allows a user to generate a simple logo as a svg file.
+
+## Installation
+
+Clone repository and install npm packages inquirer version 8.2.4 and jest. 
+
+## Usage
+
+Use "node index.js" to bring up prompts on the logo options. Once prompts have been answered, an SVG file will populate in the folder.
+
+Walkthrough video here: https://watch.screencastify.com/v/BWtStME04fsjHZYg5eUE  
+
+## Credits
+
+I referenced [this](https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Basic_Shapes#rectangle) to figure out how to create a SVG file.
+
+I also received guidance from the Xpert Learning Assistant on BCS. I also referenced code from previous class activities. 
+
+
+## License
+
+Refer to repo for license.
--- index.js
+++ index.js
@@ -3,50 +3,6 @@ const { writeFile } = require('fs').promises;
 
 const { Circle, Triangle, Square } = require('./lib/shapes');
 
-class Shape {
-  constructor(color) {
-    this.color = color;
-  }
-
-  getColor() {
-    return this.color;
-  }
-
-  render() {
-    throw new Error('Subclasses must implement the render method');
-  }
-}
-
-class Triangle extends Shape {
-  constructor(color) {
-    super(color);
-  }
-
-  render() {
-    return '<polygon points="150, 18 244, 182 56, 182" fill="' + this.getColor() + '" />';
-  }
-}
-
-class Circle extends Shape {
-  constructor(color) {
-    super(color);
-  }
-
-  render() {
-    return '<circle cx="150" cy="100" r="80" fill="' + this.getColor() + '" />';
-  }
-}
-
-class Square extends Shape {
-  constructor(color) {
-    super(color);
-  }
-
-  render() {
-    return '<rect width="200" height="200" fill="' + this.getColor() + '" />';
-  }
-}
-
 async function getUserInput() {
   const userInput = await inquirer.prompt([
     {
